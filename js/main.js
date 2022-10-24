e468edff320174204c9c037beb6ff974142a03e7
Spong3ibob <leonardomaran17@gmail.com>
2022-10-24 16:09:50 +0200

    aggiunto container

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
+
 for (var i = 1; i <= 100; i++) {
     if (i % 15 == 0) console.log("FizzBuzz");
     else if (i % 3 == 0) console.log("Fizz");
     else if (i % 5 == 0) console.log("Buzz");
-    else console.log(i);
+    const number = document.createElement("div");
+    number.classList.add("number");
+    number.innerHTML = i;
+    const container =document.getElementsByClassName("container")
+    container.append("number");
 }
+
+ 
