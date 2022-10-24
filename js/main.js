d1ad62483caae5c5df4572d99a3e278097e3ba0f
Spong3ibob <leonardomaran17@gmail.com>
2022-10-24 17:37:24 +0200

    aggiunto classe ai div e aggiunto stile

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 for (let i = 1; i <= 100; i++) {
-    if (i % 15 == 0) console.log("FizzBuzz");
-    else if (i % 3 == 0) console.log("Fizz");
-    else if (i % 5 == 0) console.log("Buzz");
-    else console.log(i);
+    let valueToShow = i;
+    if (i % 15 == 0) valueToShow = "FizzBuzz";
+    else if (i % 3 == 0) valueToShow = "Fizz";
+    else if (i % 5 == 0) valueToShow = "Buzz";
+    else valueToShow = i;
     let number = document.createElement("div");
-    let result = document.createTextNode(i);
-    
+    let result = document.createTextNode(valueToShow);
     number.appendChild(result);
     var element = document.getElementById("container");
     element.appendChild(number);
+    number.classList.add("result_stamp")
     }
 
 
